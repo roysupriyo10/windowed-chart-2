f26a6a43a3e0589c4dd1eaf7b7e6325f215b179f
Supriyo Roy <roysupriyo10@gmail.com>
2023-10-27 01:50:24 +0530

    added tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,31 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: ['IBM Plex Sans', 'sans-serif'],
+    },
+    extend: {
+      colors: {
+        'white': '#cccccc',
+        'positive': "#089981",
+        'positive-secondary': '#66BB6A',
+        'negative-secondary': '#E61D62',
+        'negative': "#F23645",
+        'main': '#1c1c1c'
+      },
+      gridTemplateColumns: {
+        "main": "85% auto",
+        "control-sliders": "1.4em 1.4em auto",
+      },
+      gridTemplateRows: {
+        "main": "minmax(25em, 46%) 40px auto",
+        "control-sliders": "auto 68px",
+      }
+    },
   },
   plugins: [],
 }
-
